a04a52b1435eefafe7675cceca426beea2456ac4
Mikko De Los Reyes <mikkode212@gmail.com>
2022-10-25 13:43:57 -0400

    completed InstuctorsNames kata

---
+++ InstructorsNames.js
@@ -0,0 +1,36 @@
+/* Create a function named instructorWithLongestName that will receive an array of instructor objects, and return the object that has the longest name. If there are two instructors with the longest name, return the first one. */
+
+const instructorWithLongestName = function(instructors) {
+  let longestName = "";
+  let instructor;
+  for (let i = 0; i < instructors.length; i++) {
+    if (instructors[i].name.length > longestName.length) {
+      longestName = instructors[i].name;
+    }
+  }
+  for (let i = 0; i < instructors.length; i++) {
+    if (instructors[i].name === longestName) {
+      instructor = instructors[i];
+    }
+  }
+  return instructor;
+};
+
+console.log(instructorWithLongestName([
+  {name: "Samuel", course: "iOS"},
+  {name: "Jeremiah", course: "Web"},
+  {name: "Ophilia", course: "Web"},
+  {name: "Donald", course: "Web"}
+]));
+console.log(instructorWithLongestName([
+  {name: "Matthew", course: "Web"},
+  {name: "David", course: "iOS"},
+  {name: "Domascus", course: "Web"}
+]));
+
+/*
+EXPECTED OUTPUT
+
+{name: "Jeremiah", course: "Web"}
+{name: "Domascus", course: "Web"}
+*/
